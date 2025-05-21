36be542fa7e160c71102d76210e7a01e196fab71
zorrumeysa <zorrumeysa54@gmail.com>
2025-05-21 08:44:29 +0300

    MVC Online Ticari Otomasyon
    Udemy Projesi

---
+++ MvcOnlineTicariOtomasyon/Controllers/GaleriController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class GaleriController : Controller
+    {
+        Context c = new Context();  
+        // GET: Galeri
+        public ActionResult Index()
+        {
+            var degerler = c.Uruns.ToList();
+            return View(degerler);
+        }
+        //public ActionResult About()
+    }
+}
